969f42be8d06e899d9d5895fef16d0ef7e86ba85
Adeel_Asghar05 <adeelji.730@gmail.com>
2026-02-22 14:04:12 +0500

    adding button

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -167,7 +167,7 @@ const categories = [
     //   { name: 'YOLO (v5-v8)', level: 95 },
     //   { name: 'OpenCV', level: 90 },
       { name: 'Object Detection', level: 90 },
-    //   { name: 'Semantic Segmentation', level: 85 },
+      { name: 'Semantic Segmentation', level: 85 },
       { name: 'Grad-CAM', level: 85 },
     ],
   },
